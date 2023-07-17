7fe1c2a5fc05c5fc0a6649770a0e1480dc25e95a
RoamingHeart <aggoden@gmail.com>
2023-07-17 18:08:42 -0400

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,10 @@ require('dotenv').config(); // Import sensitive data from .env
 
 const sequelize = process.env.JAWSDB_URL 
   ? new Sequelize(process.env.JAWSDB_URL) // JAWSDB_URL is for HEROKU
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // This is for Local
+  : new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, { 
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
